265d43ea1d712d20e6f6959e43a023b3358e4958
Side-On <nathanhighams@gmail.com>
2022-03-26 16:55:47 +0000

    Added some basic JavaScript variable testing

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// How to swap variable values
+var a = 3;
+var b = 8;
+
+console.log("a is " + a)
+console.log("b is " + b)
+
+// How to make a = 8 and b = 3, without using numbers
+var c = a;
+a = b;
+b = c;
+
+// Display the results
+console.log("a is " + a)
+console.log("b is " + b)
+
